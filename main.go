7c4aa46acec217564c8ba871d178c5a80ae021ee
Xander03 <vovakorzun92@gmail.com>
2018-04-25 17:01:31 +0300

    cors

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import (
 
 	"net/http"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
-
 
 	rootRouter := mux.NewRouter()
 	router.NewUserRouter(rootRouter.PathPrefix("/users").Subrouter())
 	router.NewTaskRouter(rootRouter.PathPrefix("/tasks").Subrouter())
 
-	http.ListenAndServe(":3000", rootRouter)
+	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"PUT", "GET", "OPTIONS", "DELETE", "POST"})
+
+	http.ListenAndServe(":8080", handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(rootRouter))
 }
